1504b5cba33d3e2ab72efd108b881621b5ce1d13
bmeistergeneral <brecon.morgan@gmail.com>
2023-07-11 16:57:21 -0400

    Created SortedIntList and modified add method

--- ListTest.java
+++ ListTest.java
@@ -9,7 +9,17 @@ public class ListTest
     public static void main(String[] args)
     {
         IntList myList = new IntList(10);
-        myList.add(100); myList.add(50); myList.add(200); myList.add(25);
+        myList.add(100);
+        myList.add(50);
+        myList.add(200);
+        myList.add(25);
         System.out.println(myList);
+
+        SortedIntList myList2 = new SortedIntList(10);
+        myList2.add(100);
+        myList2.add(50);
+        myList2.add(200);
+        myList2.add(25);
+        System.out.println(myList2);
     }
 }
--- SortedIntList.java
+++ SortedIntList.java
@@ -9,13 +9,29 @@ public class SortedIntList extends IntList {
     // Adds an integer to the list. If the list is full,
     // prints a message and does nothing.
     // ------------------------------------------------------------
-    public void add(int value)
-    {
-        if (numElements == list.length)
-            System.out.println("Can't add, list is full"); else
-        {
-            list[numElements] = value;
-            numElements++;
+    public void add(int value) {
+        if (numElements == list.length) {
+            System.out.println("Can't add, list is full");
+        } else {
+            int insertLocation = numElements; // by default at the end
+
+            for (int i = 0; i < numElements; i++) {
+                if (value <= list[i]) {
+                    insertLocation = i;
+                    break;
+                }
+            }
+
+            if (insertLocation < numElements) {
+                for (int i = numElements; i > insertLocation; i--) {
+                   list[i] = list[i - 1];
+                }
+                list[insertLocation] = value;
+                numElements++;
+            } else {
+                list[numElements] = value;
+                numElements++;
+            }
         }
     }
 }
